0fdf9a98c966dd9b70661d9aab746a7985aac107
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-08-21 21:15:08 +0700

    add server below: +web RTC with socket io server configuration +update on server/app.js

--- server/app.js
+++ server/app.js
@@ -11,10 +11,50 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-io.on("connection", (socket) => {});
+let connectedPeers = new Map()
+
+io.on("connection", (socket) => {
+  socket.emit('connection-success', {success: `/webrtcPeer#${socket.id}`})
+
+  connectedPeers.set(socket.id, socket)
+
+  socket.on('disconnect', () =>{
+    console.log('disconnected')
+    connectedPeers.delete(socket.id)
+  })
+
+  socket.on('offerOrAnswer', (data) => {
+    //kirim ke peer lain kalau ada
+    for(const [socketID, socket] of connectedPeers.entries()){
+      //jangan kirimke sendiri
+      if(socketID !== data.socketID){
+        console.log(socketID, data.payload.type)
+        socket.emit('offerOrAnswer', data.payload)
+      }
+    }
+  })
+
+  socket.on('candidate', (data) => {
+    //kirim ke peer lain kalau ada
+    for(const [socketID, socket] of connectedPeers.entries()){
+      //jangan kirimke sendiri
+      if(socketID !== data.socketID){
+        console.log(socketID, data.payload.type)
+        socket.emit('candidate', data.payload)
+      }
+    }
+  })
+
+  socket.on("move", function (url) {
+    socket.broadcast.emit("move");
+  });
+  socket.on("teks", function (txt) {
+    socket.broadcast.emit("teks", txt);
+  });
+});
 
 if (app.get("env") === "development") {
-  app.listen(PORT, function () {
+  server.listen(PORT, function () {
     console.log(`Now running on PORT ${PORT}`);
   });
 }
