1bd30242d0921a3f9aac98bcc944eb9e66d6a630
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2020-08-21 18:23:22 +0700

    + add username validation (username.length at least 6) + add password validation (password.length at least 8) + add test for username and password validation + add store + add route in app.js + add action in store + set display for login and register + add index.js on pages + set localstorage after login

--- client/src/pages/LoginPages.js
+++ client/src/pages/LoginPages.js
@@ -1,67 +1,82 @@
 import React, { useState } from "react";
-// import { gql, useMutation } from "@apollo/client";
-// import { GET_MOVIES } from "./Movies";
-import { useHistory, useRouteMatch } from "react-router-dom";
+import { useHistory, useRouteMatch, Link } from "react-router-dom";
+import { postLogin } from "../store/actions/userAction";
+import { useDispatch } from "react-redux";
 
 export default () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
-
+  const dispatch = useDispatch();
   const logo = require("../chess-pack/chess-king-white.png");
 
-  //   const onchange = (e) => {
-  //     let { name, value } = e.target;
-  //     const newMoviesInput = { ...MoviesInput, [name]: value };
-  //     setMovieInput(newMoviesInput);
-  //   };
+  const usernameLoginHandler = (e) => {
+    setUsername(e.target.value);
+  };
+  const passwordLoginHandler = (e) => {
+    setPassword(e.target.value);
+  };
 
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     MoviesInput.popularity = Number(MoviesInput.popularity);
-  //     addMovies({
-  //       variables: {
-  //         addNewMovies: MoviesInput,
-  //       },
-  //     });
-  //     history.push("/main-menu");
-  //   };
+  const formLoginHandler = (e) => {
+    e.preventDefault();
+    const loginUser = {
+      username,
+      password,
+    };
+    dispatch(postLogin(loginUser, history));
+  };
 
   return (
-    <div className="motherLogin">
-      <div>
-        <img className="LogoLogin" src={logo} />
-        <h1 className="Login-Title">Welcome TO ChessMater</h1>
+    <>
+      <div style={{ backgroundColor: "#263554" }}>
+        <div
+          className="text-center"
+          style={{ paddingTop: "2%", color: "white" }}
+        >
+          <img
+            className="LogoLogin"
+            src={logo}
+            style={{
+              width: "15%",
+            }}
+          />
+          <h1 className="Login-Title">Welcome TO ChessMaster</h1>
+        </div>
         <br />
-
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="text"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
-        <p>Dont have account ye ? Register down Here !!</p>
-        <button type="submit" className="btn btn-primary" value="save">
-          Submit
-        </button>
+        <div style={{ margin: "0 31%", color: "white", paddingBottom: "2.4%" }}>
+          <form onSubmit={(e) => formLoginHandler(e)}>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e) => usernameLoginHandler(e)}
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                onChange={(e) => passwordLoginHandler(e)}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="btn btn-info"
+                style={{ width: "100%" }}
+              >
+                Login
+              </button>
+            </div>
+          </form>
+          <p style={{ marginTop: "10%" }}>
+            Already have account ? <Link to="/register">click here</Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
