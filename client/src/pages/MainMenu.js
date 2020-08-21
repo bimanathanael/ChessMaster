a148298847767a4f402a17a151c4541575f3c300
wibyB <wibybramantyo123@gmail.com>
2020-08-21 17:36:02 +0700

    adding create room using modal in main menu

--- client/src/pages/MainMenu.js
+++ client/src/pages/MainMenu.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import Cards from "../components/Cards";
+import { Button, Modal } from "react-bootstrap";
 
 export default () => {
   const history = useHistory();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const kingW = require("../chess-pack/chess-king-white.png");
   const kingB = require("../chess-pack/chess-king-black.png");
@@ -13,15 +19,62 @@ export default () => {
       <button className="btn btn-danger buttonLogout">Logout</button>
       <h1>Main Menu</h1>
       <div className="buttonGroup">
-        <button className="btn btn-primary buttoncreate" type="button">
+        <Button variant="primary" onClick={handleShow}>
           Create Room
-        </button>
+        </Button>
         <button className="btn btn-primary ToLeaderBoard" type="button">
           Leader Board
         </button>
       </div>
       <br />
       <Cards />
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Create Room</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Room Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">apalah</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Rating Select</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
