b91e36f2d9792a137191e0e4b306858e050242aa
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2020-08-26 00:10:30 +0700

    add history game page to client

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as LoginPages } from "./LoginPages";
 export { default as MainMenu } from "./MainMenu";
 export { default as RegisterPage } from "./RegisterPage";
 export { default as LeaderBoard } from "./LeaderBoard";
+export { default as HistoryGame } from "./HistoryGame";
